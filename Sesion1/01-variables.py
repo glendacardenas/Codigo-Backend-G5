97d9cc304a866048d6def37cf825a07fffe3b422
GLENDA YANINA CARDENAS VILCA <gycardenas@yahoo.com.mx>
2021-12-10 20:11:17 -0500

    chore : declaracion de variables

--- Sesion1/01-variables.py
+++ Sesion1/01-variables.py
@@ -1,3 +1,31 @@
 
 # variable numerica 
 numero = 10
+numero2 =10.5
+#numero ="Febrero"
+nombre = "Eduardo"
+fecha = 0
+print ('hola :)')
+print(type(numero2))
+print(type(numero))
+
+soltero=True
+calor=True
+print(type(soltero))
+
+# variables que tienen varios valores
+
+#arreglos son listas list
+
+edades= [10, 12 , 40 , 60, 'Eduardo', 14.5 , False, [1,2]]
+print(edades)
+
+#JSON (JavaScript Object Notation )/diccionario
+curso={
+    'nombre': 'Backend',
+    'nombre': 'Frontend',
+
+    'dificultad':'Dificil',
+
+}
+print (curso['dificultad'] )
