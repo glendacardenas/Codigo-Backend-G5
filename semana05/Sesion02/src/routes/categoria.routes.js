e287bcbfad66434f25fbfedaa572d04f556f752b
GLENDA YANINA CARDENAS VILCA <gycardenas@yahoo.com.mx>
2022-01-25 16:35:54 -0500

    chore: test sin express

---
+++ semana05/Sesion02/src/routes/categoria.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+    actualizarCategoria,
+    buscarCategoria,
+    crearCategoria,
+    listarCategoria,
+} from "../controllers/categoria.controller.js";
+
+export const categoriaRouter = Router();
+
+categoriaRouter.route("/categoria").post(crearCategoria).get(listarCategoria);
+
+categoriaRouter.get("/buscarCategoria", buscarCategoria);
+
+categoriaRouter.route("/categoria/:id").put(actualizarCategoria);
---
+++ semana05/index.spec.js
@@ -0,0 +1,5 @@
+it('deberia sumar 1+1', () => {
+    const x = 1 + 1;
+    expect(x).toBe(2);
+
+})
